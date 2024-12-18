d8b4d51838ac53326b46eb9bb9147983bb6aff53
Boardy <adam@boardy.uk>
2024-12-18 10:37:36 +0000

    10. Setting up vue router and routerlink

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <RouterLink to="/">Go to home</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
