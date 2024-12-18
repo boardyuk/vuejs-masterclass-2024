ca83ba9ce8cbc0be5d191ff8899d6d76ed9da3b4
Boardy <adam@boardy.uk>
2024-12-18 14:12:16 +0000

    18. Secure secret data with env variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'https://vjchdhibyirugjuolwjw.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqY2hkaGlieWlydWdqdW9sd2p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ1MzAwMjQsImV4cCI6MjA1MDEwNjAyNH0.OopQPt8z2UbzDkVCte134Eanw7vyhzis3_uJMF0Vf7Y',
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY,
 )
