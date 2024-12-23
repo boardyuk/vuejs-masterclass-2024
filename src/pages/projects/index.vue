69ecd0d84982fc73f1ecc4c70a4017816470b47b
Boardy <adam@boardy.uk>
2024-12-23 09:53:13 +0000

    31. Added typescript support to supabase

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
 import { ref } from 'vue'
+import type { Tables } from '../../../database/types'
 
-const projects = ref()
+const projects = ref<Tables<'projects'>[] | null >(null)
 
 // IIFE (Immediately Invoked Function Expression)
 ;(async () => {
@@ -14,14 +15,17 @@ const projects = ref()
 
   console.log('Projects: ', projects.value)
 })()
-
 </script>
 
 <template>
   <div>
     <h1>Projects Page</h1>
     <RouterLink to="/">Go to home</RouterLink>
-    {{ projects }}
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
