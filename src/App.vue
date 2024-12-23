9dc119c5491ddd28c73a8a1daddde19e664d9081
Boardy <adam@boardy.uk>
2024-12-23 12:51:07 +0000

    36. Lucide Icons with Iconify

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,18 @@ import {
 <template>
   <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
     <form class="w-full max-w-96">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <circle cx="11" cy="11" r="8" />
+          <path d="m21 21l-4.3-4.3" />
+        </g>
+      </svg>
       <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
     </form>
 
