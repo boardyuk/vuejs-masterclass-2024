def78d40a521035d0884793a6c6213180f579f27
Boardy <adam@boardy.uk>
2024-12-30 12:33:20 +0000

    54. Handle Complex Supabase Queries

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
-import type { Tables } from '../../../database/types'
 import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
+import type { QueryData } from '@supabase/supabase-js'
 
 usePageStore().pageData.title = 'My Tasks'
 
-const tasks = ref<Tables<'tasks'>[] | null>(null)
+const tasksWithProjectsQuery = supabase.from('tasks').select(`*, projects (id, name, slug)`)
+type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
 
-// IIFE (Immediately Invoked Function Expression)
+const tasks = ref<TasksWithProjects | null>(null)
+
 const getTasks = async () => {
-  const { data, error } = await supabase.from('tasks').select(`*, projects (id, name, slug)`)
+  const { data, error } = await tasksWithProjectsQuery
 
   if (error) console.log(error)
 
@@ -19,7 +21,7 @@ const getTasks = async () => {
 
 await getTasks()
 
-const columns: ColumnDef<Tables<'tasks'>>[] = [
+const columns: ColumnDef<TasksWithProjects[0]>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
@@ -52,14 +54,16 @@ const columns: ColumnDef<Tables<'tasks'>>[] = [
     accessorKey: 'projects',
     header: () => h('div', { class: 'text-left' }, 'Project'),
     cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.projects.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full',
-        },
-        () => row.getValue('projects').name,
-      )
+      return row.original.projects
+        ? h(
+            RouterLink,
+            {
+              to: `/projects/${row.original.projects.slug}`,
+              class: 'text-left font-medium hover:bg-muted block w-full',
+            },
+            () => row.original.projects?.name,
+          )
+        : ''
     },
   },
   {
